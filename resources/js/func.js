b59c4b4bdde0d37a50fe3e0ccda7f52d896aae02
trinhson7122 <son07012002@gmail.com>
2023-04-27 21:26:03 +0700

    Create func.js

---
+++ resources/js/func.js
@@ -0,0 +1,32 @@
+function removeAccents(str) {
+    var AccentsMap = [
+        "aàảãáạăằẳẵắặâầẩẫấậ",
+        "AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ",
+        "dđ",
+        "DĐ",
+        "eèẻẽéẹêềểễếệ",
+        "EÈẺẼÉẸÊỀỂỄẾỆ",
+        "iìỉĩíị",
+        "IÌỈĨÍỊ",
+        "oòỏõóọôồổỗốộơờởỡớợ",
+        "OÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ",
+        "uùủũúụưừửữứự",
+        "UÙỦŨÚỤƯỪỬỮỨỰ",
+        "yỳỷỹýỵ",
+        "YỲỶỸÝỴ",
+    ];
+    for (var i = 0; i < AccentsMap.length; i++) {
+        var re = new RegExp("[" + AccentsMap[i].substr(1) + "]", "g");
+        var char = AccentsMap[i][0];
+        str = str.replace(re, char);
+    }
+    return str;
+}
+
+function makeShortName(str) {
+    str = removeAccents(str);
+    const arr = str.split(" ");
+    const shortName = arr[0][0] + arr[arr.length - 1][0];
+    return shortName;
+}
+export { removeAccents, makeShortName };
