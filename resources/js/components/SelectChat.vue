58310135d408e1ab3f4357ccfae0d51c76851e58
trinhson7122 <son07012002@gmail.com>
2023-05-23 20:34:53 +0700

    Update SelectChat.vue

--- resources/js/components/SelectChat.vue
+++ resources/js/components/SelectChat.vue
@@ -1,14 +1,14 @@
 <template>
-    <li> <!-- class="active" -->
+    <li class="list_user" @click="setActive"> <!-- class="active" -->
         <a href="javascript:void(0);">
-            <div class="media">
+            <div class="media align-items-center">
                 <div class="chat-user-img online align-self-center mr-3 online">
                     <div v-if="toUser.avatar">
                         <img :src="toUser.avatar" alt="" class="rounded-circle avatar-xs">
                         <span class="user-status"></span>
                     </div>
                     <div v-else class="avatar-xs">
-                        <span class="avatar-title rounded-circle bg-soft-primary text-primary">TS</span>
+                        <span class="avatar-title rounded-circle bg-soft-primary text-primary">{{ toUser.short_name }}</span>
                         <span class="user-status"></span>
                     </div>
                 </div>
@@ -16,11 +16,15 @@
                     <h5 class="text-truncate font-size-15 mb-1"> 
                         {{ toUser.name }}
                     </h5>
-                    <p class="chat-user-message text-truncate mb-0"> 
+                    <p v-if="read" class="chat-user-message text-truncate mb-0"> 
+                        {{ (user.last_user_id_send == this.$store.state.auth.user.id ) ? "Bạn: " : ""}}
                         {{ user.last_message }}
                     </p>
+                    <p v-else class="chat-user-message text-truncate mb-0 fw-bold"> 
+                       {{ user.last_message }}
+                    </p>
                 </div>
-                <!-- <div class="font-size-11">05 min</div> -->
+                <div v-show="!read" class="font-size-11"><i class="fa-solid fa-circle" style="color: #045df6;"></i></div>
             </div>
         </a>
     </li>
@@ -35,6 +39,17 @@ export default {
             toUser: null,
         };
     },
+    computed: {
+        read() {
+            if (this.user.last_user_id_send != this.$store.state.auth.user.id) {
+                if (this.user.from_user_id == this.$store.state.auth.user.id) {
+                    return this.user.is_from_user_read;
+                }
+                return this.user.is_to_user_read;
+            }
+            return true;
+        },
+    },
     created() {
         if (this.user.from_user_id == this.$store.state.auth.user.id) {
             this.toUser = this.user.to_user;
@@ -42,6 +57,15 @@ export default {
         else {
             this.toUser = this.user.from_user;
         }
+    },
+    methods: {
+        setActive() {
+            const list_users = document.querySelectorAll('li.list_user');
+            list_users.forEach((item) => {
+                item.classList.remove('active');
+            });
+            this.$el.classList.add('active');
+        },
     },
 };
 </script>
