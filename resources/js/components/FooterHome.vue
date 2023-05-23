a78436df5c114acd310e529d3c0847793051ab83
trinhson7122 <son07012002@gmail.com>
2023-05-23 20:34:46 +0700

    Update FooterHome.vue

--- resources/js/components/FooterHome.vue
+++ resources/js/components/FooterHome.vue
@@ -1,11 +1,5 @@
 <template>
     <div class="side-menu flex-lg-column mr-lg-1">
-        <!-- <div class="navbar-brand-box"><a href="/" aria-current="page"
-                class="logo logo-dark router-link-exact-active router-link-active"><span class="logo-sm"><img
-                        src="/img/logo.de6401ef.svg" alt="" height="30"></span></a><a href="/" aria-current="page"
-                class="logo logo-light router-link-exact-active router-link-active"><span class="logo-sm"><img
-                        src="/img/logo.de6401ef.svg" alt="" height="30"></span></a>
-        </div> -->
         <div class="flex-lg-column my-auto">
             <ul role="tablist" class="nav nav-pills side-menu-nav justify-content-center">
                 <li data-placement="top" title="Profile" class="nav-item">
@@ -18,33 +12,22 @@
                         <i class="fa-solid fa-user"></i>
                     </router-link>
                 </li>
-                <li>
-                    <div @click="onLogout">logout</div>
-                </li>
-                <div class="dropdown b-dropdown nav-item profile-user-dropdown d-inline-block d-lg-none dropup btn-group"
-                    id="__BVID__37">
-                    <button data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button"
+                <div class="dropdown b-dropdown nav-item profile-user-dropdown d-inline-block d-lg-none dropup btn-group">
+                    <button data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button"
                         class="btn dropdown-toggle btn-white nav-link justify-content-center align-items-center d-flex"
-                        id="dd1">
+                        id="dropdown-user-menu1">
                         <img v-if="user.avatar" :src="user.avatar" alt="" class="profile-user rounded-circle">
                         <div v-else class="avatar-xs text-center">
                             <span class="avatar-title rounded-circle bg-soft-primary text-primary">{{ user.short_name
                             }}</span>
                         </div>
                     </button>
-                    <ul role="menu" tabindex="-1" class="dropdown-menu" aria-labelledby="dd1" style="">
-                        <li role="presentation"><a role="menuitem" href="javascript:void(0);" target="_self"
-                                class="dropdown-item"> Profile <i class="ri-profile-line float-right text-muted"></i></a>
-                        </li>
-                        <li role="presentation"><a role="menuitem" href="javascript:void(0);" target="_self"
-                                class="dropdown-item"> Settinga <i
-                                    class="ri-settings-3-line float-right text-muted"></i></a>
-                        </li>
+                    <ul role="menu" tabindex="" class="dropdown-menu" aria-labelledby="dropdown-user-menu1">
                         <li role="presentation">
-                            <hr role="separator" aria-orientation="horizontal" class="dropdown-divider">
+                            <a @click="onLogout" role="menuitem" href="javascript:void(0);" target="_self" class="dropdown-item align-items-center d-flex">
+                                Đăng xuất <i class="fa-solid fa-right-from-bracket float-right text-muted ml-3"></i>
+                            </a>
                         </li>
-                        <li role="presentation"><a role="menuitem" href="/logout" target="_self" class="dropdown-item"> Log
-                                out <i class="ri-logout-circle-r-line float-right text-muted"></i></a></li>
                     </ul>
                 </div>
             </ul>
@@ -52,28 +35,18 @@
         <div class="flex-lg-column d-none d-lg-block">
             <ul class="nav side-menu-nav justify-content-center">
                 <div class="dropdown nav-item btn-group dropup profile-user-dropdown btn-group">
-                    <button data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button"
-                        class="btn dropdown-toggle btn-white" id="dropdownMenuButton1">
+                    <button data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button"
+                        class="btn dropdown-toggle btn-white" id="dropdown-user-menu">
                         <img v-if="user.avatar != null" :src="user.avatar" alt="" class="profile-user rounded-circle">
                         <div v-else class="avatar-xs text-center">
                             <span class="avatar-title rounded-circle bg-soft-primary text-primary">{{ user.short_name
                             }}</span>
                         </div>
                     </button>
-                    <ul role="menu" tabindex="" class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <ul role="menu" tabindex="" class="dropdown-menu" aria-labelledby="dropdown-user-menu">
                         <li role="presentation">
-                            <a role="menuitem" href="javascript:void(0);" target="_self" class="dropdown-item"> Profile
-                                <i class="ri-profile-line float-right text-muted"></i>
-                            </a>
-                        </li>
-                        <li role="presentation">
-                            <a role="menuitem" href="javascript:void(0);" target="_self" class="dropdown-item"> Setting
-                                <i class="ri-settings-3-line float-right text-muted"></i>
-                            </a>
-                        </li>
-                        <li role="presentation">
-                            <a role="menuitem" href="javascript:void(0);" target="_self" class="dropdown-item">
-                                Logout <i class="ri-logout-circle-r-line float-right text-muted"></i>
+                            <a @click="onLogout" role="menuitem" href="javascript:void(0);" target="_self" class="dropdown-item align-items-center d-flex">
+                                Đăng xuất <i class="fa-solid fa-right-from-bracket float-right text-muted ml-3"></i>
                             </a>
                         </li>
                     </ul>
@@ -83,7 +56,7 @@
     </div>
 </template>
 <script>
-
+import { mapActions } from "vuex";
 export default {
     data() {
         return {
@@ -94,8 +67,11 @@ export default {
         this.user = this.$store.state.auth.user;
     },
     methods: {
+        ...mapActions({
+            logout: "auth/logout",
+        }),
         onLogout() {
-            this.$store.dispatch('auth/logout');
+            this.logout();
         }
     }
 };
